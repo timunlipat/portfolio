5c2a363076e027e0f78f6af730b53ae6ecdf3884
Zamil <zamilaziz92@gmail.com>
2024-11-04 12:23:06 +0800

    add the layout for the Navbar

--- src/app/component/navbar.jsx
+++ src/app/component/navbar.jsx
@@ -2,9 +2,10 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import Image from 'next/image';
 
 const links = [
-    { url: '/', title: 'home' },
+    { url: '/', title: 'Home' },
     { url: '/about', title: 'About' },
     { url: '/porfolio', title: 'Porfolio' },
     { url: '/contact', title: 'Contact' },
@@ -15,35 +16,70 @@ const Navbar = () => {
 
     return (
         <div className='h-full flex justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+            {/* Links */}
+            <div className='hidden md:flex gap-4 w-1/3'>
+                {links.map(link => (
+                    <Link href={link.url} key={link.title}>
+                        {link.title}
+                    </Link>
+                ))}
+            </div>
             {/* Logo */}
-            <Link
-                href='/'
-                className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'
-            >
-                <span className='text-white mr-1'>Zamil</span>
-                <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>
-                    .dev
-                </span>
-            </Link>
+            <div className='md:hidden lg:flex w-1/3 justify-center'>
+                <Link
+                    href='/'
+                    className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'
+                >
+                    <span className='text-white mr-1'>Zamil</span>
+                    <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>
+                        .dev
+                    </span>
+                </Link>
+            </div>
+            {/* Social */}
+            <div className='hidden md:flex gap-4 w-1/3'>
+                <Link href='/'>
+                    <Image src='/github.png' alt='' width={24} height={24} />
+                </Link>
+                <Link href='/'>
+                    <Image src='/dribbble.png' alt='' width={24} height={24} />
+                </Link>
+                <Link href='/'>
+                    <Image src='/instagram.png' alt='' width={24} height={24} />
+                </Link>
+                <Link href='/'>
+                    <Image src='/facebook.png' alt='' width={24} height={24} />
+                </Link>
+                <Link href='/'>
+                    <Image src='/pinterest.png' alt='' width={24} height={24} />
+                </Link>
+                <Link href='/'>
+                    <Image src='/linkedin.png' alt='' width={24} height={24} />
+                </Link>
+            </div>
 
             {/* Menu */}
-            <button
-                className='w-10 h-8 flex flex-col justify-between z-50 relative'
-                onClick={() => setOpen(prev => !prev)}
-            >
-                <div className='w-10 h-1 bg-white rounded'></div>
-                <div className='w-10 h-1 bg-white rounded'></div>
-                <div className='w-10 h-1 bg-white rounded'></div>
-            </button>
+            <div className='md:hidden'>
+                <button
+                    className='w-10 h-8 flex flex-col justify-between z-50 relative'
+                    onClick={() => setOpen(!open)}
+                >
+                    <div className='w-10 h-1 bg-white rounded'></div>
+                    <div className='w-10 h-1 bg-white rounded'></div>
+                    <div className='w-10 h-1 bg-white rounded'></div>
+                </button>
 
-            {/* Menu List */}
-            {open && (
-                <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
-                    {links.map(link => (
-                        <Link href={link.url}>{link.title}</Link>
-                    ))}
-                </div>
-            )}
+                {/* Menu List */}
+                {open && (
+                    <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40'>
+                        {links.map(link => (
+                            <Link href={link.url} key={link.title}>
+                                {link.title}
+                            </Link>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
