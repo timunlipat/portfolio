2960ba0f8a02e09d2819a5fa4f18fcf0a610a316
Zamil <zamilaziz92@gmail.com>
2024-11-05 09:49:11 +0800

    add signature to aboutpage

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 const About = () => {
     return (
@@ -15,8 +16,10 @@ const About = () => {
                 <div className='p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64'>
                     {/* Biography Container */}
                     <div className='flex flex-col gap-12 justify-center'>
-                        <h1>BIOGRAPHY</h1>
-                        <p>
+                        {/* Biography Title */}
+                        <h1 className='font-bold text-2xl'>BIOGRAPHY</h1>
+                        {/* Biography Description */}
+                        <p className='text-lg'>
                             Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit, sed do eiusmod tempor incididunt ut labore et
                             dolore magna aliqua. Ut enim ad minim veniam, quis
@@ -25,11 +28,20 @@ const About = () => {
                             reprehenderit in voluptate velit esse cillum dolore
                             eu fugiat nulla pariatur.
                         </p>
-                        <span>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit.
+                        {/* Biography Quote */}
+                        <span className='italic'>
+                            Time waits for no man. Unless that man is me.
                         </span>
-                        <div className=''></div>
+                        {/* Biography Sign */}
+                        <div className='self-end'>
+                            <Image
+                                src='/signature-chuck.png'
+                                alt=''
+                                height={75}
+                                width={185}
+                            />
+                        </div>
+                        {/* Biography Scroll */}
                     </div>
                     {/* Skills Container */}
                     <div className=''>SKILLS</div>
