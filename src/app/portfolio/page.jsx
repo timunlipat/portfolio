bd68f42bacc1ff574cd519323e9d9e7dbdef8178
Zamil <zamilaziz92@gmail.com>
2024-11-26 21:17:44 +0800

    add layout for portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,12 @@
 'use client';
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 const Portfolio = () => {
+    const ref = useRef();
+    const { scrollYProgress } = useScroll({ target: ref });
+    const x = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+
     return (
         <motion.div
             className='h-full'
@@ -9,7 +14,12 @@ const Portfolio = () => {
             animate={{ y: '0%' }}
             transition={{ duration: 1 }}
         >
-            Portfolio
+            <div className='h-[600vh]' ref={ref}>
+                <div className='w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center'>
+                    My Works
+                </div>
+                <div className='sticky top-0 flex h-screen gap-4 items-center'></div>
+            </div>
         </motion.div>
     );
 };
